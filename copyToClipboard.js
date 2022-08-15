85d4ab2003fd43ce4bf3fa5a26318e4cafba601c
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2022-08-15 13:05:59 +0900

    Modified to give both Windows and Mac paths. Fixed so that URLs can be copied even when a file is open.

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -1,30 +1,25 @@
 var global = window;
 global.COPY_TO_CLIPBOARD = global.COPY_TO_CLIPBOARD || {};
-
-global.COPY_TO_CLIPBOARD.getBoxDocumentPWD = function(joinStr){
-    const url = Box.prefetchedData['/app-api/enduserapp/current-user'].preview.appHost + 'app-api/enduserapp/folder/' + document.URL.split('/').pop();
+global.COPY_TO_CLIPBOARD.getBoxDocumentPWD = function(targetFolderId){
+    const url = Box.prefetchedData['/app-api/enduserapp/current-user'].preview.appHost + 'app-api/enduserapp/folder/' + targetFolderId;
     return fetch(url)
         .then(response => response.json())
         .then(json => json.folder.path.map(i => i.name).filter((_, idx) => idx > 0));
 };
-
-global.COPY_TO_CLIPBOARD.copyToClipboard = async function() {
-    const arrayFolder = await this.getBoxDocumentPWD();
-    const joinInfo = this.getPathJoinInfo();
-    const joinFolderName = arrayFolder.join(joinInfo.joinStr);
-    const res = joinInfo.prtPath + joinFolderName + '\n' + document.URL;
-    navigator.clipboard.writeText(res);
+global.COPY_TO_CLIPBOARD.copyToClipboard = async function(){
+    const arrayTarget = Box.postStreamURLs;
+    const isFolder = arrayTarget.some(x => new RegExp('folder').test(x));
+    const targetItems = Box.postStreamData[arrayTarget[0]].items[0];
+    const folderId = isFolder ? document.URL.split('/').pop() : targetItems.parentFolderID;
+    const arrayFolder = await this.getBoxDocumentPWD(folderId);
+    const joinFolderName = arrayFolder.join('/');
+    const folderAndFileName = !isFolder ? joinFolderName + '/' + targetItems.name : joinFolderName;
+    const arrayFolders = ['%USERPROFILE%/Box/', '~/Library/CloudStorage/Box-Box/'].map(x => x + folderAndFileName + '\n');
+    const res = arrayFolders.join('') + document.URL;
+    console.log(res);
     return;
 };
-global.COPY_TO_CLIPBOARD.getPathJoinInfo = function(){
-    const osInfo = navigator.userAgentData.platform;
-    let res = {};
-    res.joinStr = '/';
-    if (osInfo === 'Windows') {
-        res.prtPath = '%USERPROFILE%/Box/';
-    } else {
-        res.prtPath = '~/Library/CloudStorage/Box-Box/';
-    };
-    return res;
+global.DETERMINE_FOLDER_OR_FILE = function(targetUrl){
+    return new RegExp('folder').test(targetUrl);
 };
 global.COPY_TO_CLIPBOARD.copyToClipboard();
