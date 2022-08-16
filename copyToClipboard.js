7add2aaf2f477b2c2b67e1a35ae4a0eeb6cbac26
MarikoOhtsuka <mariko.ohtsuka@nnh.go.jp>
2022-08-16 11:01:52 +0900

    Fixed to copy title and URL if it works outside Box web.

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -20,11 +20,23 @@ global.COPY_TO_CLIPBOARD.createTargetFilePath = function(targetFileId){
 global.COPY_TO_CLIPBOARD.getBoxFolderPath = function(json){
     return json.folder.path.map(i => i.name).filter((_, idx) => idx > 0);
 };
+global.COPY_TO_CLIPBOARD.getUrlInfo=function(){
+    let a = new String(document.title);
+    a.allReplace = function(a){
+        let b=this,c;
+        for(c in a)b=b.replace(new RegExp(c,"g"),a[c]);return b
+    }.bind(a);
+    return a.allReplace({":":"\uff1a","\\[":"\uff3b","\\]":"\uff3d"})+"\n"+document.URL;
+};
+global.COPY_TO_CLIPBOARD.copyTextAndTitle=function(){
+    navigator.clipboard.writeText(this.getUrlInfo());
+};
 global.COPY_TO_CLIPBOARD.copyToClipboard = async function(){
     try {
         const temp = Box.prefetchedData['/app-api/enduserapp/current-user'];
     } catch(error) {
         console.log('Does not work outside Box web.');
+        this.copyTextAndTitle();
         return;
     };
     const fileId = document.URL.split('/').pop();
